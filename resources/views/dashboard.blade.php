99e67ba18e99fec86f3e8c02f745221830ed6833
bartek <bartek.szyma@gmail.com>
2020-09-16 20:08:38 +0200

    add jetstream, telescope and migrations

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,30 @@
+@if (Auth::user()->activated === 0)
+    <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+        <h1 style="display: grid; padding-top: 50px; text-align: center; font-size: x-large; font-style: oblique;">Pana Pawła aplikacja coming soon</h1>
+    </x-app-layout>
+
+
+@else
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+        </x-slot>
+        <div>
+
+        </div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <x-jet-welcome />
+                </div>
+            </div>
+        </div>
+    </x-app-layout>
+@endif
